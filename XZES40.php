e9ef0d18ed8b41770f6fba482cdd2ace2ddae55b
zixunlu <luzi@oregonstate.edu>
2017-03-05 15:07:16 -0800

    web interface
    The temporary website page is http://web.engr.oregonstate.edu/~luzi/XZES40/XZES40.php

---
+++ XZES40.php
@@ -0,0 +1,39 @@
+
+<head>
+<meta content="en-us" http-equiv="Content-Language">
+<style type="text/css">
+.auto-style1 {
+	font-size: xx-large;
+}
+.auto-style2 {
+	margin-bottom: 0px;
+}
+.auto-style3 {
+	background-color: #FF6300;
+}
+.auto-style4 {
+	font-size: large;
+}
+</style>
+</head>
+
+<p class="auto-style1"><span class="auto-style3">XZES40-Transformation</span>
+<img alt="" class="auto-style2" height="76" src="apache.PNG" width="354">
+<img alt="" height="74" src="osu.PNG" width="399"></p>
+<hr>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<p class="auto-style4">&nbsp;XML Transformor </p>
+<p class="auto-style4">1. Select your XML file to convert</p>
+<p class="auto-style4">&nbsp;<input name="Button1" style="width: 375px" type="button" value="Select your file"></p>
+<p class="auto-style4">2.Select your XSLT file to convert</p>
+&nbsp;&nbsp;&nbsp;
+<input name="Button2" style="width: 375px" type="button" value="Select your file"><p class="auto-style4">
+3. Convert your file&nbsp;</p>
+<p class="auto-style4">&nbsp;&nbsp;
+<input name="Button3" style="width: 375px" type="button" value="Transfor Now"></p>
+<p class="auto-style4">&nbsp;</p>
+<p class="auto-style4">&nbsp;</p>
+<p class="auto-style4">&nbsp;</p>
+<p>&nbsp;</p>
+<hr>
+<p>&nbsp;Copyright The Oregon State University Computer Science 2016-2017 
+Capstone team. </p>
